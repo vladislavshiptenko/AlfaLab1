91db2c50208b182a29dbaaeb3a6fe817d0abcc58
shiptenkovlad <shiptenkovlad@yandex-team.ru>
2025-04-08 11:47:45 +0300

    initial

---
+++ Lab1/Sources/Models/BookGenre.swift
@@ -0,0 +1,15 @@
+//
+//  BookGenre.swift
+//  Lab1
+//
+//  Created by Vladislav Shiptenko on 02.04.2025.
+//
+
+enum BookGenre: String {
+    case fiction = "Fiction"
+    case mystery = "Mystery"
+    case sciFi = "Science Fiction"
+    case biography = "Biography"
+    case history = "History"
+    case education = "Education"
+}
